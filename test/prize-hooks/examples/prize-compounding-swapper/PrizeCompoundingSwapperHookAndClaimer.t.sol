c4550e2dc0f9ad7e229c879d4dbb5b9eba74ffba
Trevor Richard <trmidpoint@gmail.com>
2024-09-16 15:58:01 +0000

    add prize compounding swapper tests

--- test/prize-hooks/examples/prize-compounding-swapper/PrizeCompoundingSwapperHookAndClaimer.t.sol
+++ test/prize-hooks/examples/prize-compounding-swapper/PrizeCompoundingSwapperHookAndClaimer.t.sol
@@ -5,6 +5,7 @@ import { Test } from "forge-std/Test.sol";
 
 import {
     PrizeCompoundingSwapperHookAndClaimer,
+    PrizeCompoundingSwapperHook,
     PrizeVault,
     IUniV3Oracle,
     ISwapperFactory,
@@ -14,13 +15,14 @@ import {
     QuoteParams,
     QuotePair
 } from "src/prize-hooks/examples/prize-compounding-swapper/PrizeCompoundingSwapperHookAndClaimer.sol";
+import { PrizePool } from "pt-v5-prize-pool/PrizePool.sol";
 import { PrizeHooks } from "pt-v5-vault/interfaces/IPrizeHooks.sol";
 
 contract PrizeCompoundingSwapperHookAndClaimerTest is Test {
 
     uint256 fork;
-    uint256 forkBlock = 19424769;
-    uint256 forkTimestamp = 1725638885;
+    uint256 forkBlock = 19393711;
+    uint256 forkTimestamp = 1725576769;
 
     PrizeCompoundingSwapperHookAndClaimer public hookAndClaimer;
 
@@ -38,6 +40,18 @@ contract PrizeCompoundingSwapperHookAndClaimerTest is Test {
 
     address public alice;
 
+    uint8 public winTier = 5;
+    address[] public winners = [
+        address(0x08DE07fE4B53F315A1C93cA8396c18985432d41E),
+        address(0xd682fb159a2cBC47DE97A046D76391dDae104988)
+    ];
+    uint32[] public winner0PrizeIndices = [417, 766];
+    uint32[] public winner1PrizeIndices = [258];
+    uint32[][] public prizeIndices = [
+        winner0PrizeIndices,
+        winner1PrizeIndices
+    ];
+
     function setUp() public {
         fork = vm.createFork('base', forkBlock);
         vm.selectFork(fork);
@@ -50,6 +64,10 @@ contract PrizeCompoundingSwapperHookAndClaimerTest is Test {
             przUSDC,
             defaultScaleFactor
         );
+
+        vm.startPrank(przUSDC.owner());
+        przUSDC.setClaimer(address(hookAndClaimer));
+        vm.stopPrank();
     }
 
     function testQuoteWethToPrzWeth() external {
@@ -91,7 +109,7 @@ contract PrizeCompoundingSwapperHookAndClaimerTest is Test {
             data: ""
         });
         uint256[] memory amounts = hookAndClaimer.getQuoteAmounts(quoteParams);
-        assertApproxEqAbs(amounts[0], 2291e6, 10e6);
+        assertApproxEqAbs(amounts[0], 2366e6, 10e6);
     }
 
     function testQuoteWethToUsdc() external {
@@ -105,7 +123,207 @@ contract PrizeCompoundingSwapperHookAndClaimerTest is Test {
             data: ""
         });
         uint256[] memory amounts = hookAndClaimer.getQuoteAmounts(quoteParams);
-        assertApproxEqAbs(amounts[0], 2291e6, 10e6);
+        assertApproxEqAbs(amounts[0], 2366e6, 10e6);
+    }
+
+    function testSetSwapperDoesNothingIfSet() external {
+        vm.startPrank(alice);
+        assertEq(hookAndClaimer.swappers(alice), address(0));
+
+        vm.expectEmit(true, false, true, false);
+        emit PrizeCompoundingSwapperHook.SetSwapper(alice, address(0), address(0));
+        ISwapper swapper = hookAndClaimer.setSwapper();
+        assertEq(hookAndClaimer.swappers(alice), address(swapper));
+        assertNotEq(address(swapper), address(0));
+
+        ISwapper swapperSame = hookAndClaimer.setSwapper();
+        assertEq(hookAndClaimer.swappers(alice), address(swapper));
+        assertEq(address(swapperSame), address(swapper));
+        vm.stopPrank();
+    }
+
+    function testRemoveAndRecoverSwapper() external {
+        vm.startPrank(alice);
+        assertEq(hookAndClaimer.swappers(alice), address(0));
+        ISwapper swapper = hookAndClaimer.setSwapper();
+        assertEq(hookAndClaimer.swappers(alice), address(swapper));
+        assertEq(swapper.owner(), address(hookAndClaimer));
+
+        vm.expectEmit();
+        emit PrizeCompoundingSwapperHook.SetSwapper(alice, address(0), address(swapper));
+        address oldSwapper = hookAndClaimer.removeAndRecoverSwapper();
+        assertEq(oldSwapper, address(swapper));
+        assertEq(swapper.owner(), alice);
+        assertEq(hookAndClaimer.swappers(alice), address(0));
+        vm.stopPrank();
+    }
+
+    function testSetPrizeSizeVote() external {
+        vm.startPrank(alice);
+        assertEq(hookAndClaimer.prizeSizeVotes(alice), 0);
+        vm.expectEmit();
+        emit PrizeCompoundingSwapperHook.SetPrizeSizeVote(alice, 1e6);
+        hookAndClaimer.setPrizeSizeVote(1e6);
+        assertEq(hookAndClaimer.prizeSizeVotes(alice), 1e6);
+        vm.stopPrank();
+    }
+
+    function testSetPrizeSizeVote_setBackToZero() external {
+        vm.startPrank(alice);
+        assertEq(hookAndClaimer.prizeSizeVotes(alice), 0);
+        hookAndClaimer.setPrizeSizeVote(1e6);
+        assertEq(hookAndClaimer.prizeSizeVotes(alice), 1e6);
+
+        vm.expectEmit();
+        emit PrizeCompoundingSwapperHook.SetPrizeSizeVote(alice, 0);
+        hookAndClaimer.setPrizeSizeVote(0);
+        assertEq(hookAndClaimer.prizeSizeVotes(alice), 0);
+        vm.stopPrank();
+    }
+
+    function testPrizeSizeVoting() external {
+        vm.startPrank(alice);
+
+        uint8 numTiers = hookAndClaimer.prizePool().numberOfTiers();
+        uint8 canaryLow = numTiers - 2;
+        uint8 canaryHigh = numTiers - 1;
+
+        // no vote set, no revert
+        hookAndClaimer.beforeClaimPrize(alice, canaryLow, uint32(0), uint96(0), address(0));
+        hookAndClaimer.beforeClaimPrize(alice, canaryHigh, uint32(0), uint96(0), address(0));
+
+        // vote really small prize ($0.05), no revert either
+        hookAndClaimer.setPrizeSizeVote(0.05e6);
+        hookAndClaimer.beforeClaimPrize(alice, canaryLow, uint32(0), uint96(0), address(0));
+        hookAndClaimer.beforeClaimPrize(alice, canaryHigh, uint32(0), uint96(0), address(0));
+
+        // vote near current prize size ($0.50), revert high, but not low
+        hookAndClaimer.setPrizeSizeVote(0.5e6);
+        hookAndClaimer.beforeClaimPrize(alice, canaryLow, uint32(0), uint96(0), address(0));
+        try hookAndClaimer.beforeClaimPrize(alice, canaryHigh, uint32(0), uint96(0), address(0)) returns (address, bytes memory) {
+            // nothing
+            revert("should have reverted");
+        } catch (bytes memory reason) {
+            bytes4 errorSelector = abi.decode(reason, (bytes4));
+            assertEq(errorSelector, PrizeCompoundingSwapperHook.VoteToStayAtCurrentPrizeTiers.selector);
+            (bool success, bytes memory data) = address(this).staticcall(reason);
+            (uint8 currentTiers, uint256 dailyPrizeSize, uint256 minDesiredDailyPrizeSize) = abi.decode(data, (uint8,uint256,uint256));
+            assertEq(currentTiers, numTiers);
+            assertEq(minDesiredDailyPrizeSize, 0.5e6);
+            assertGt(dailyPrizeSize, minDesiredDailyPrizeSize);
+        }
+
+        // vote higher than current prize size ($20.00), revert both
+        hookAndClaimer.setPrizeSizeVote(20e6);
+        try hookAndClaimer.beforeClaimPrize(alice, canaryLow, uint32(0), uint96(0), address(0)) returns (address, bytes memory) {
+            // nothing
+            revert("should have reverted");
+        } catch (bytes memory reason) {
+            bytes4 errorSelector = abi.decode(reason, (bytes4));
+            assertEq(errorSelector, PrizeCompoundingSwapperHook.VoteToLowerPrizeTiers.selector);
+            (bool success, bytes memory data) = address(this).staticcall(reason);
+            (uint8 currentTiers, uint256 dailyPrizeSize, uint256 minDesiredDailyPrizeSize) = abi.decode(data, (uint8,uint256,uint256));
+            assertEq(currentTiers, numTiers);
+            assertEq(minDesiredDailyPrizeSize, 20e6);
+            assertLt(dailyPrizeSize, minDesiredDailyPrizeSize);
+        }
+        try hookAndClaimer.beforeClaimPrize(alice, canaryHigh, uint32(0), uint96(0), address(0)) returns (address, bytes memory) {
+            // nothing
+            revert("should have reverted");
+        } catch (bytes memory reason) {
+            bytes4 errorSelector = abi.decode(reason, (bytes4));
+            assertEq(errorSelector, PrizeCompoundingSwapperHook.VoteToLowerPrizeTiers.selector);
+            (bool success, bytes memory data) = address(this).staticcall(reason);
+            (uint8 currentTiers, uint256 dailyPrizeSize, uint256 minDesiredDailyPrizeSize) = abi.decode(data, (uint8,uint256,uint256));
+            assertEq(currentTiers, numTiers);
+            assertEq(minDesiredDailyPrizeSize, 20e6);
+            assertLt(dailyPrizeSize, minDesiredDailyPrizeSize);
+        }
+        
+        vm.stopPrank();
+    }
+
+    function testPrizeSizeVoting_OracleRevertFallback() external {
+        vm.startPrank(alice);
+
+        uint8 numTiers = hookAndClaimer.prizePool().numberOfTiers();
+        uint8 canaryLow = numTiers - 2;
+        uint8 canaryHigh = numTiers - 1;
+
+        (bool _success, bytes memory _tempData) = address(baseOracle).staticcall(abi.encodeWithSignature("sequencerFeed()"));
+        assertEq(_success, true);
+        address _sequencer = abi.decode(_tempData, (address));
+        vm.mockCallRevert(address(_sequencer), abi.encodeWithSignature("latestRoundData()"), "revert");
+
+        hookAndClaimer.setPrizeSizeVote(1e6);
+        (address recipient, bytes memory data) = hookAndClaimer.beforeClaimPrize(alice, canaryLow, uint32(0), uint96(0), address(0));
+        assertEq(recipient, alice);
+        assertEq(keccak256(data), keccak256("vote-abstained"));
+
+        vm.stopPrank();
+    }
+
+    function testClaimPrizesWithCompounding_swappersNotSetForClaims() external {
+        vm.expectEmit();
+        emit PrizeCompoundingSwapperHookAndClaimer.SwapperNotSetForWinner(winners[0]);
+        vm.expectEmit();
+        emit PrizeCompoundingSwapperHookAndClaimer.SwapperNotSetForWinner(winners[1]);
+        vm.expectRevert(abi.encodeWithSelector(PrizeCompoundingSwapperHookAndClaimer.MinRewardNotMet.selector, 0, 1));
+        hookAndClaimer.claimPrizes(winTier, winners, prizeIndices, alice, 1);
+    }
+
+    function testClaimPrizesWithCompounding() external {
+        vm.startPrank(winners[0]);
+        hookAndClaimer.setSwapper();
+        przUSDC.setHooks(PrizeHooks(true, false, hookAndClaimer));
+        vm.stopPrank();
+        vm.startPrank(winners[1]);
+        hookAndClaimer.setSwapper();
+        przUSDC.setHooks(PrizeHooks(true, false, hookAndClaimer));
+        vm.stopPrank();
+
+        uint256 winner0PrzUsdcBalanceBefore = przUSDC.balanceOf(winners[0]);
+        uint256 winner1PrzUsdcBalanceBefore = przUSDC.balanceOf(winners[1]);
+
+        uint256 totalReward = hookAndClaimer.claimPrizes(winTier, winners, prizeIndices, alice, 1);
+        assertGt(totalReward, 7.5e12);
+        assertEq(IERC20(WETH).balanceOf(alice), totalReward);
+
+        assertGt(przUSDC.balanceOf(winners[0]), winner0PrzUsdcBalanceBefore + 1e6); // more than $1 in prize value added
+        assertGt(przUSDC.balanceOf(winners[1]), winner1PrzUsdcBalanceBefore + 0.5e6); // more than $0.50 in prize value added
+    }
+
+    function testClaimPrizesFallbackPeriod() external {
+        // only set one winner's hooks to test both work in fallback period
+        vm.startPrank(winners[0]);
+        hookAndClaimer.setSwapper();
+        przUSDC.setHooks(PrizeHooks(true, false, hookAndClaimer));
+        vm.stopPrank();
+
+        uint256 drawClosed = hookAndClaimer.prizePool().drawClosesAt(hookAndClaimer.prizePool().getLastAwardedDrawId());
+        uint256 drawPeriod = hookAndClaimer.prizePool().drawPeriodSeconds();
+        vm.warp(drawClosed + (drawPeriod * 3) / 4 + 1); // in fallback period
+
+        uint256 snap = vm.snapshot();
+
+        uint256 totalReward = hookAndClaimer.claimPrizes(winTier, winners, prizeIndices, alice, 1);
+        assertGt(totalReward, 7.5e12); // should still be around 1% in start of fallback period
+        assertEq(IERC20(WETH).balanceOf(alice), totalReward);
+
+        vm.revertTo(snap);
+        vm.warp(drawClosed + drawPeriod - 1); // end of fallback period
+
+        totalReward = hookAndClaimer.claimPrizes(winTier, winners, prizeIndices, alice, 1);
+        assertGt(totalReward, 7.5e13); // should be around 10% at end of fallback period
+        assertEq(IERC20(WETH).balanceOf(alice), totalReward);
+    }
+
+    // helpers for decoding error data
+    function VoteToStayAtCurrentPrizeTiers(uint8 currentTiers, uint256 dailyPrizeSize, uint256 minDesiredDailyPrizeSize) external view returns (uint8,uint256,uint256) {
+        return (currentTiers, dailyPrizeSize, minDesiredDailyPrizeSize);
+    }
+    function VoteToLowerPrizeTiers(uint8 currentTiers, uint256 dailyPrizeSize, uint256 minDesiredDailyPrizeSize) external view returns (uint8,uint256,uint256) {
+        return (currentTiers, dailyPrizeSize, minDesiredDailyPrizeSize);
     }
 
 }
